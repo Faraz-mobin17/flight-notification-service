48e8f0c8dd16ac64b791aae56aa7390a67cd434d
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-08-05 20:04:21 +0530

    added db migrations and enums

---
+++ src/utils/enums.js
@@ -0,0 +1,9 @@
+const TICKET_STATUS = {
+  PENDING: "PENDING",
+  SUCCESS: "SUCCESS",
+  FAILED: "FAILED",
+};
+
+module.exports = {
+  TICKET_STATUS,
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 module.exports = {
-}
+  Enums: require("./enums"),
+};
